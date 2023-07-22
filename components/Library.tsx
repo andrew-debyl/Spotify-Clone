1ff2faf5cdb3671a6d418dc96cf8f11840d78962
Andrew <andrewdebyl@gmail.com>
2023-07-22 18:16:55 -0400

    changes

--- components/Library.tsx
+++ components/Library.tsx
@@ -2,10 +2,27 @@
 
 import { TbPlaylist } from "react-icons/tb";
 import { AiOutlinePlus } from "react-icons/ai";
+import useAuthModal from "@/hooks/useAuthModal";
+import { useUser } from "@/hooks/useUser";
+import useUploadModal from "@/hooks/useUploadModal";
+import { Song } from "@/types";
+import MediaItem from "./MediaItem";
 
-const Library = () => {
+interface Props {
+  songs: Song[]
+}
+
+const Library: React.FC<Props> = ({songs}) => {
+  const authModal = useAuthModal()
+  const uploadModal = useUploadModal()
+  const {user} = useUser()
+
   const onClick = () => {
-    //handle upload later
+    if (!user) {
+      return authModal.onOpen();
+    }
+//check for sub
+    return uploadModal.onOpen()
   };
 
   return (
@@ -22,7 +39,9 @@ const Library = () => {
         />
       </div>
       <div className="flex flex-col gap-y-2 mt-4 px-3">
-        List of songs
+        {songs.map((item) => (
+          <MediaItem onClick={()=>{}} key={item.id} data={item}/>
+        ))}
       </div>
     </div>
   );
